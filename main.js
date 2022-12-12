47cd35bec0040cce711f4a3ca84ade6189295e4c
Dylan Spyer <dylanspyer@gmail.com>
2022-12-12 16:53:44 -0600

    fixed bug to make chaining operations work

--- main.js
+++ main.js
@@ -1,38 +1,130 @@
-function add(...args) {
-  return args.reduce(function (a, b) {
-    return a + b;
-  }, 0);
+// function add(...args) {
+//   return args.reduce(function (a, b) {
+//     return a + b;
+//   }, 0);
+// }
+
+// function subtract(...args) {
+//   return args.reduce(function (a, b) {
+//     return a - b;
+//   }, 0);
+// }
+
+// function multiply(...args) {
+//   return args.reduce(function (a, b) {
+//     return a * b;
+//   }, 0);
+// }
+
+// function divide(...args) {
+//   return args.reduce(function (a, b) {
+//     return a / b;
+//   }, 0);
+// }
+
+// function operate(operator, ...args) {
+//   switch (operator) {
+//     case "+":
+//       return add(...args);
+//     case "-":
+//       return subtract(...args);
+//     case "×":
+//       return multiply(...args);
+//     case "÷":
+//       return divide(...args);
+//   }
+let displayValue = "0";
+let operator = null;
+let firstOperand = null;
+let secondOperand = null;
+let result = null;
+
+const btn = document.querySelectorAll(".btn");
+const numberBtn = document.querySelectorAll(".number-btn");
+const operatorBtn = document.querySelectorAll(".operator-btn");
+const divideBtn = document.querySelector(".divide");
+const multiplyBtn = document.querySelector(".multiply");
+const addBtn = document.querySelector(".add");
+const subtractBtn = document.querySelector(".subtract");
+const equalsBtn = document.querySelector(".equals");
+const clearBtn = document.querySelector(".clear-button");
+const output = document.querySelector(".output");
+
+function add(a, b) {
+  return a + b;
 }
 
-function subtract(...args) {
-  return args.reduce(function (a, b) {
-    return a - b;
-  }, 0);
+function subtract(a, b) {
+  return a - b;
 }
 
-function multiply(...args) {
-  return args.reduce(function (a, b) {
-    return a * b;
-  }, 0);
+function divide(a, b) {
+  return a / b;
 }
 
-function divide(...args) {
-  return args.reduce(function (a, b) {
-    return a / b;
-  }, 0);
+function multiply(a, b) {
+  return a * b;
 }
 
-function operate(operator, ...args) {
+function operate(a, b, operator) {
   switch (operator) {
     case "+":
-      return add(...args);
+      return add(a, b);
     case "-":
-      return subtract(...args);
-    case "*":
-      return multiply(...args);
-    case "/":
-      return divide(...args);
+      return subtract(a, b);
+    case "×":
+      return multiply(a, b);
+    case "÷":
+      return divide(a, b);
   }
 }
 
-console.log(operate("*", 20, 2, 2));
+function handleClick() {
+  if (operator === null) {
+    firstOperand = parseInt(output.textContent);
+  } else if (operator != null && result === null) {
+    secondOperand = parseInt(output.textContent);
+  } else if (result != null) {
+    firstOperand = result;
+    secondOperand = Number(displayValue);
+  }
+}
+
+function clear() {
+  output.textContent = "";
+  firstOperand = null;
+  secondOperand = null;
+  operator = null;
+  result = null;
+  console.log(firstOperand);
+  console.log(secondOperand);
+  console.log(result);
+}
+
+clearBtn.onclick = () => clear();
+
+equalsBtn.onclick = function () {
+  result = Number(operate(firstOperand, secondOperand, operator));
+  output.textContent = result;
+  console.log(firstOperand);
+  console.log(secondOperand);
+  console.log(result);
+};
+
+numberBtn.forEach((btn) => {
+  btn.addEventListener("click", function (event) {
+    output.textContent += event.target.innerText;
+    displayValue = output.textContent;
+    handleClick();
+  });
+});
+
+operatorBtn.forEach((btn) => {
+  btn.addEventListener("click", function (event) {
+    operator = event.target.innerText;
+    output.textContent = "";
+  });
+});
+
+//Bug - if you evaluate an equation and then add numbers to the result, it doesn't change firstOperand to include the numbers you added:
+//How it should work: get rid of what's on the screen and start a new equation if you press a number
