2c440c52771dd56f0c445a03bf3f4ee9c419ef11
Dylan Spyer <dylanspyer@gmail.com>
2022-12-14 21:03:30 -0600

    organized code, deleted comments

--- main.js
+++ main.js
@@ -1,43 +1,6 @@
-// function add(...args) {
-//   return args.reduce(function (a, b) {
-//     return a + b;
-//   }, 0);
-// }
-
-// function subtract(...args) {
-//   return args.reduce(function (a, b) {
-//     return a - b;
-//   }, 0);
-// }
-
-// function multiply(...args) {
-//   return args.reduce(function (a, b) {
-//     return a * b;
-//   }, 0);
-// }
-
-// function divide(...args) {
-//   return args.reduce(function (a, b) {
-//     return a / b;
-//   }, 0);
-// }
-
-// function operate(operator, ...args) {
-//   switch (operator) {
-//     case "+":
-//       return add(...args);
-//     case "-":
-//       return subtract(...args);
-//     case "×":
-//       return multiply(...args);
-//     case "÷":
-//       return divide(...args);
-//   }
-let displayValue = "0";
 let operator = "";
 let currentValue = "";
 let previousValue = "";
-let result = "";
 
 const numberBtn = document.querySelectorAll(".number-btn");
 const operatorBtn = document.querySelectorAll(".operator-btn");
@@ -45,8 +8,8 @@ const equalsBtn = document.querySelector(".equals");
 const clearBtn = document.querySelector(".clear-btn");
 const deleteBtn = document.querySelector(".delete-btn");
 const decimalBtn = document.querySelector(".decimal");
-let mainOutput = document.querySelector(".main-output");
-let secondaryOutput = document.querySelector(".secondary-output");
+const mainOutput = document.querySelector(".main-output");
+const secondaryOutput = document.querySelector(".secondary-output");
 
 function add(a, b) {
   return a + b;
@@ -88,8 +51,6 @@ function deleteLast() {
   mainOutput.textContent = currentValue;
 }
 
-deleteBtn.onclick = () => deleteLast();
-
 function roundNumber(num) {
   return Math.round(num * 1000) / 1000;
 }
@@ -113,19 +74,32 @@ function addDecimal() {
   }
 }
 
-decimalBtn.onclick = () => addDecimal();
-
 function clear() {
   mainOutput.textContent = "";
   secondaryOutput.textContent = "";
   previousValue = "";
   currentValue = "";
   operator = "";
-  result = "";
 }
 
+deleteBtn.onclick = () => deleteLast();
+decimalBtn.onclick = () => addDecimal();
 clearBtn.onclick = () => clear();
-
+numberBtn.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    handleNumber(e.target.textContent);
+    mainOutput.textContent = currentValue;
+  });
+});
+operatorBtn.forEach((op) => {
+  op.addEventListener("click", function (e) {
+    if (currentValue != "") {
+      handleOperator(e.target.textContent);
+      secondaryOutput.textContent = `${previousValue} ${operator}`;
+      mainOutput.textContent = currentValue;
+    }
+  });
+});
 equalsBtn.onclick = function () {
   if (currentValue != "" && previousValue != "") {
     operate();
@@ -137,26 +111,6 @@ equalsBtn.onclick = function () {
       mainOutput.textContent = previousValue.slice(0, 5) + "...";
     }
   }
-  console.log(currentValue);
-  console.log(previousValue);
 };
-
-numberBtn.forEach((btn) => {
-  btn.addEventListener("click", function (e) {
-    handleNumber(e.target.textContent);
-    mainOutput.textContent = currentValue;
-  });
-});
-
-operatorBtn.forEach((op) => {
-  op.addEventListener("click", function (e) {
-    handleOperator(e.target.textContent);
-    secondaryOutput.textContent = `${previousValue} ${operator}`;
-    mainOutput.textContent = currentValue;
-  });
-});
-
-//Bug - if you evaluate an equation and then add numbers to the result, it doesn't change previousValue to include the numbers you added:
-//How it should work: get rid of what's on the screen and start a new equation if you press a number
 
 //Bug - After you have a currentValue and currentValue, if you press an operator button instead of equals, the equation doesn't get evaluated.
